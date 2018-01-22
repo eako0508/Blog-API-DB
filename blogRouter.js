8751264b934b29225ebe12739420e27fa3e37aa3
eako0508 <eako0508@gmail.com>
2018-01-21 19:39:51 -0500

    rough draft

--- blogRouter.js
+++ blogRouter.js
@@ -40,8 +40,34 @@ router.post('/blog-posts', jsonParser, (req,res) => {
   res.status(201).json(result);
 });
 //update
+router.put('/blog-posts/:id', jsonParser, (req,res) => {
+    const req_param = ['title', 'content', 'author', 'date'];
+    for(let i=0;i<req_param.length;i++){
+        const req_elem = req_param[i];
+        if(!(req_elem in req.body)){
+            const msg = `Required field ${req_elem} is missing.`;
+            console.error(msg);
+            res.status(401).send(msg); 
+        }
+    }
 
+    if(req.param.id !== req.body.id){
+        const msg = 'id is not matching.';
+        console.error(msg);
+        res.status(401).send(msg);
+    }
+
+    const result = BlogPosts.update(req.body);
+    res.status(204).end();
+});
 
 
 
 //delete
+router.delete('/blog-posts/:id', jsonParser, (req,res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog ${req.params.id}.`);
+    res.status(204).end();
+});
+
+module.exports = router;
