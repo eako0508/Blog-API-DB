8e67edb92b75cdda8013c6090affcf6fa1195be0
eako0508 <eako0508@gmail.com>
2018-01-23 10:06:26 -0500

    completed get

--- test/test-server.js
+++ test/test-server.js
@@ -1,11 +1,13 @@
 const chai = require('chai');
 const chaiHttp = require('chai-Http');
 
+const {app, runServer, closeServer} = require('../server');
+
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-const {app, runServer, closeServer} = require('../server');
+
 
 describe('Blog post', function(){
 
@@ -16,15 +18,20 @@ describe('Blog post', function(){
 		return closeServer();
 	});
 
-	it('should grab items on GET', function(res){
+	it('should grab items on GET', function(){
 		return chai.request(app)
-			.get('/blog-post')
+			.get('/blog-posts')
 			.then(function(res){
 				expect(res).to.be.status(200);
 				expect(res).to.be.json;
 				expect(res.body).to.be.a('array');
-				//expect(res.body).to.include('title', 'content', 'author');
 				expect(res.body.length).to.be.at.least(1);
+				const requried_field = ['title', 'content', 'author'];
+				res.body.forEach(function(element){
+					expect(element).to.be.a('object');
+					expect(element).to.include.keys(requried_field);
+				})
+				
 			});
 	});
 
