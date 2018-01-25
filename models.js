555c68b88812547f910561a510966be1a5d0a2bb
Eugene <eako0508@gmail.com>
2018-01-25 17:50:00 -0500

    fixed undefined for firstName

--- models.js
+++ models.js
@@ -6,7 +6,7 @@ const blogSchema = mongoose.Schema({
   title: {type: String, required: true},
   content: {type: String, required: true},
   author: {
-	firstname: {type: String, required: true},
+	firstName: {type: String, required: true},
 	lastName: {type: String, required: true}
   },
   publishDate: {type: String}
