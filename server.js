e9b2500e2c65ca8c10836619c69e96afb01c0b7b
eako0508 <eako0508@gmail.com>
2018-01-25 13:02:49 -0500

    get get/id put

--- server.js
+++ server.js
@@ -1,19 +1,61 @@
-
+const bodyParser = require('body-parser');
 const express = require('express');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 mongoose.Promise = global.Promise;
 
 const { PORT, DATABASE_URL } = require('./config');
-const { Restaurant } = require('./models');
-
-const blogRouter = require('./blogRouter');
+const { BlogPosts } = require('./models');
 
 const app = express();
 app.use(morgan('common'));
+app.use(bodyParser.json());
 
-app.use('/blog-posts', blogRouter);
+//app.use('/blog-posts', blogRouter);
+
+app.get('/blog-posts', (req,res) =>{
+  BlogPosts
+    .find()
+    .limit(10)
+    .then(blogs => {
+      res.json(blogs);
+    })
+    .catch(err=>{
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error'});
+    });
+});
+
+app.get('/blog-posts/:id', (req,res)=>{
+  BlogPosts
+    .findById(req.params.id)
+    .then(blogs => res.json(blogs))
+    .catch(err=>{
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error'});
+    });
+});
+
+app.put('/blog-posts/:id', (req,res)=>{
+  if(!(req.params.id && req.body.id && req.params.id === req.body.id)){
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({message: message}); 
+  }
+  
+  const updatedEntry = {};
+  const updateFields = ['title', 'content', 'author', 'publishDate'];
+  updateFields.forEach(field =>{
+    if(field in req.body){
+      updatedEntry[field] = req.body[field];
+    }
+  });
+  BlogPosts
+    .findByIdAndUpdate(req.params.id, {$set: updatedEntry})
+});
 
 let server;
 
